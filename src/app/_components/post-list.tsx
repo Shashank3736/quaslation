798998ab6623eccda9135cd49a207fb2e8848442
Shreyash Raj <shreyashr267@gmail.com>
2024-07-30 21:23:43 +0530

    updated ui

--- src/app/_components/post-list.tsx
+++ src/app/_components/post-list.tsx
@@ -1,8 +1,11 @@
 "use client"
 
 import H3 from "@/components/typography/h3"
+import Muted from "@/components/typography/muted"
 import { Button } from "@/components/ui/button"
 import { getLatestPosts, LatestPosts } from "@/lib/actions"
+import { timeAgo } from "@/lib/utils"
+import Link from "next/link"
 import { useState } from "react"
 
 export default function PostList({ posts }:{ posts: LatestPosts}) {
@@ -17,15 +20,19 @@ export default function PostList({ posts }:{ posts: LatestPosts}) {
     }))).finally(() => setLoading(false))
   }
   return (
-    <div>
+    <div className="flex flex-col p-4">
       {chapters.chapters.map((chapter) => (
-        <div key={chapter.id}>
-          <H3>Chapter {chapter.chapter}: {chapter.title}</H3>
-          <p>{chapter.description}</p>
+        <div key={chapter.id} className="p-4 mb-4 border rounded-lg cursor-pointer" onClick={() => window.location.href=`/chapter/${chapter.id}`}>
+          <H3 className="mb-2">Chapter {chapter.chapter}: {chapter.title}</H3>
+          <p className="mb-2">{chapter.description}<Link className="text-blue-600 dark:text-blue-400 hover:underline" href={`/chapter/${chapter.id}`}> Read More {">>"}</Link></p>
+          <div className="flex justify-between">
+            <Muted>Novel: {chapter.novel.title}</Muted>
+            <Muted>{timeAgo(chapter.publishedAt)}</Muted>
+          </div>
         </div>
       ))}
       {chapters.chapters.length !== chapters.chaptersConnection.aggregate.count && (
-        <Button disabled={loading} onClick={() => loadMore({ skip: chapters.chapters.length })}>{loading? "Loading...": "Load More"}</Button>
+        <Button className="center self-center" disabled={loading} onClick={() => loadMore({ skip: chapters.chapters.length })}>{loading? "Loading...": "Load More"}</Button>
       )}
     </div>
   )
