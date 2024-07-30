ec88b61884d77b851536deee6395646ebe2aa245
Shreyash Raj <shreyashr267@gmail.com>
2024-07-30 21:23:59 +0530

    improved ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import { getLatestPosts } from "@/lib/actions";
 import PostList from "./_components/post-list";
+import H2 from "@/components/typography/h2";
+import { Separator } from "@/components/ui/separator";
 
 export default async function Home() {
   const posts = await getLatestPosts({})
   return (
     <main>
-      <h1>Latest Chapters</h1>
+      <H2 className="text-center">Latest Chapters</H2>
+      <Separator />
       <PostList posts={posts} />
     </main>
   );
