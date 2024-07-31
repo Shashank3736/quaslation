3f514837300dbfe646c209c6011700c71e045284
Shreyash Raj <shreyashr267@gmail.com>
2024-07-31 20:44:23 +0530

    Updated with loading effect

--- src/app/_components/post-list.tsx
+++ src/app/_components/post-list.tsx
@@ -3,15 +3,16 @@
 import H3 from "@/components/typography/h3"
 import Muted from "@/components/typography/muted"
 import { Button } from "@/components/ui/button"
+import { Skeleton } from "@/components/ui/skeleton"
 import { ToastAction } from "@/components/ui/toast"
 import { useToast } from "@/components/ui/use-toast"
 import { ChapterDetail, getLatestPosts, LatestPosts } from "@/lib/actions"
 import { timeAgo } from "@/lib/utils"
 import Link from "next/link"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
-export default function PostList({ posts }:{ posts: LatestPosts}) {
-  const [chapters, setChapters] = useState<LatestPosts>(posts)
+export default function PostList() {
+  const [chapters, setChapters] = useState<LatestPosts>({ chaptersConnection: { aggregate: { count: 0 }}, chapters: []})
   const [loading, setLoading] = useState(false)
   const { toast } = useToast()
 
@@ -43,10 +44,14 @@ export default function PostList({ posts }:{ posts: LatestPosts}) {
       chaptersConnection: posts.chaptersConnection
     }})).finally(() => setLoading(false))
   }
+
+  useEffect(() => {
+    getLatestPosts({}).then(data => setChapters(data))
+  },[])
   
   return (
     <div className="flex flex-col p-4">
-      {chapters.chapters.map((chapter) => (
+      {chapters.chapters.length > 0 ? chapters.chapters.map((chapter) => (
         <div key={chapter.id} className="p-4 mb-4 border rounded-lg">
           <H3 className="mb-2">Chapter {chapter.chapter}: {chapter.title}</H3>
           <p className="mb-2">{chapter.description}<Link className="text-blue-600 dark:text-blue-400 hover:underline" href={`/chapter/${chapter.id}`}> Read More {">>"}</Link></p>
@@ -55,7 +60,20 @@ export default function PostList({ posts }:{ posts: LatestPosts}) {
             <Muted>{timeAgo(chapter.publishedAt)}</Muted>
           </div>
         </div>
-      ))}
+      )):(
+        <div>
+          {Array.from({ length: 10 }, (_, index) => (
+            <div key={index} className="p-4 mb-4 space-y-2 border rounded-lg">
+              <Skeleton className="h-6 w-28" />
+              <Skeleton className="h-28 w-full" />
+              <div className="flex justify-between">
+                <Skeleton className="h-3 w-16" />
+                <Skeleton className="h-3 w-16" />
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
       {chapters.chapters.length !== chapters.chaptersConnection.aggregate.count && (
         <Button className="center self-center" disabled={loading} onClick={() => loadMore({ skip: chapters.chapters.length })}>{loading? "Loading...": "Load More"}</Button>
       )}
