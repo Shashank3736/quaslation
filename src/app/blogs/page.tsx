c1934e9c57b0af960cd26ebc234673af8bf3d775
Shreyash Raj <shreyashr267@gmail.com>
2024-07-31 12:11:51 +0530

    blogs page

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,33 @@
+import H2 from '@/components/typography/h2'
+import H3 from '@/components/typography/h3'
+import Muted from '@/components/typography/muted'
+import { Button } from '@/components/ui/button'
+import { Separator } from '@/components/ui/separator'
+import { getBlogs } from '@/lib/actions'
+import { timeAgo } from '@/lib/utils'
+import Link from 'next/link'
+import React from 'react'
+
+export default async function BlogList() {
+  const blogs = await getBlogs({})
+  return (
+    <div>
+      <H2>Blogs & Updates</H2>
+      <Separator />
+      <div className='p-8 mt-4 border rounded-lg'>
+        {blogs.map(blog => (
+          <div key={blog.slug} className='flex flex-col'>
+            <H3 className='mb-2'>{blog.title}</H3>
+            <p>{blog.description}</p>
+            <div className='flex justify-between mt-4'>
+              <Button>
+                <Link href={`/blogs/${blog.slug}`}>Read Blog</Link>
+              </Button>
+              <Muted className='mt-4'>{timeAgo(blog.publishedAt)}</Muted>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
