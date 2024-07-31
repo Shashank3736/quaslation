9103d2bf572066dcffbd0b4ffa8ebcaf534290eb
Shreyash Raj <shreyashr267@gmail.com>
2024-07-31 12:50:55 +0530

    blog page

---
+++ src/app/blogs/[slug]/page.tsx
@@ -0,0 +1,18 @@
+import H1 from '@/components/typography/h1';
+import Muted from '@/components/typography/muted';
+import { Separator } from '@/components/ui/separator';
+import { getBlog } from '@/lib/actions'
+import { formatDate } from '@/lib/utils';
+import React from 'react'
+
+export default async function Blog({ params }: { params: { slug: string }}) {
+  const blog = await getBlog(params.slug);
+  return (
+    <div className='p-4 flex flex-col'>
+      <H1>{blog.title}</H1>
+      <Separator className='my-4' />
+      <div className='prose dark:prose-invert lg:prose-lg' dangerouslySetInnerHTML={{__html: blog.content.html}} />
+      <Muted className="mt-4 self-end">Date: {formatDate(new Date(blog.publishedAt))}</Muted>
+    </div>
+  )
+}
