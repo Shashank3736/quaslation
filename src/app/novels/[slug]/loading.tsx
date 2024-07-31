d8c3a21f41e1ef84b1f0bd411f6b22a3ae92d95f
Shreyash Raj <shreyashr267@gmail.com>
2024-07-31 21:20:32 +0530

    lazy loading novel

---
+++ src/app/novels/[slug]/loading.tsx
@@ -0,0 +1,44 @@
+import { Separator } from '@/components/ui/separator'
+import { Skeleton } from '@/components/ui/skeleton'
+import React from 'react'
+
+const Volume = () => {
+  return (
+    <div className='mt-2'>
+      <Skeleton className='h-3 w-32' />
+      <div className='mt-4 space-y-3'>
+        <Skeleton className="h-6 w-64"></Skeleton>
+        <Skeleton className="h-6 w-64"></Skeleton>
+        <Skeleton className="h-6 w-64"></Skeleton>
+        <Skeleton className="h-6 w-64"></Skeleton>
+        <Skeleton className="h-6 w-64"></Skeleton>
+        <Skeleton className="h-6 w-64"></Skeleton>
+        <Skeleton className="h-6 w-64"></Skeleton>
+        <Skeleton className="h-6 w-64"></Skeleton>
+        <Skeleton className="h-6 w-64"></Skeleton>
+        <Skeleton className="h-6 w-64"></Skeleton>
+        <Skeleton className="h-6 w-64"></Skeleton>
+        <Skeleton className="h-6 w-64"></Skeleton>
+        <Skeleton className="h-6 w-64"></Skeleton>
+        <Skeleton className="h-6 w-64"></Skeleton>
+        <Skeleton className="h-6 w-64"></Skeleton>
+      </div>
+    </div>
+  )
+}
+export default function loading() {
+  return (
+    <div className='p-4 space-y-3'>
+      <Skeleton className='h-12 w-full' />
+      <Skeleton className='h-32 w-full' />
+      <Separator />
+      <div className="mt-4">
+        <Volume></Volume>
+        <Volume></Volume>
+        <Volume></Volume>
+        <Volume></Volume>
+        <Volume></Volume>
+      </div>
+    </div>
+  )
+}
