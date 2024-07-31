f9b5928de28fa0fa365783c5554d42b95a68f796
Shreyash Raj <shreyashr267@gmail.com>
2024-07-31 13:13:18 +0530

    Movile friendly

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -1,5 +1,6 @@
 import { ModeToggle } from '@/components/system/dark-mode-button'
 import { Button } from '@/components/ui/button'
+import { Sheet, SheetClose, SheetContent, SheetTrigger } from '@/components/ui/sheet'
 import Link from 'next/link'
 import React from 'react'
 
@@ -9,12 +10,35 @@ export default function Navbar() {
       <div className='flex'>
         <p className='ml-2 text-2xl font-semibold'>Quaslation</p>
       </div>
-      <div className='flex space-x-2'>
+      <div className='hidden md:flex space-x-2'>
         <Button variant={"outline"} asChild><Link href={"/"}>Home</Link></Button>
         <Button variant={"outline"} asChild><Link href={"/blogs/"}>Blogs</Link></Button>
         <Button variant={"outline"} asChild><Link href={"/novels/"}>Novels</Link></Button>
         <ModeToggle />
       </div>
+      <Sheet>
+        <SheetTrigger className='md:hidden'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
+          </svg>
+        </SheetTrigger>
+        <SheetContent className='flex flex-col'>
+          <SheetClose asChild>
+            <Button variant={"outline"} asChild><Link href={"/"}>Home</Link></Button>
+          </SheetClose>
+          <SheetClose asChild>
+            <Button variant={"outline"} asChild><Link href={"/blogs/"}>Blogs</Link></Button>
+          </SheetClose>
+          <SheetClose asChild>
+            <Button variant={"outline"} asChild><Link href={"/novels/"}>Novels</Link></Button>
+          </SheetClose>
+          <SheetClose asChild>
+            <div className='self-center'>
+              <ModeToggle />
+            </div>
+          </SheetClose>
+        </SheetContent>
+      </Sheet>
     </div>
   )
 }
