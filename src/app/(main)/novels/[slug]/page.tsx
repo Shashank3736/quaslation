b92f21344dab52fa9d6dfb0b472fd5ce448250ef
Shreyash Raj <shreyashr267@gmail.com>
2024-08-07 18:50:28 +0530

    changed font size

--- src/app/(main)/novels/[slug]/page.tsx
+++ src/app/(main)/novels/[slug]/page.tsx
@@ -8,7 +8,7 @@ import VolumeChapters from './_components/volume';
 export default async function NovelPage({ params }:{ params: { slug: string} }) {
   const novel = await getNovel(params.slug);
   return (
-    <div className='p-4 md:text-lg'>
+    <div className='p-4'>
       <H2 className='text-center'>{novel.novel.title}</H2>
       <p className='my-4'>{novel.novel.description}</p>
       <Separator />
