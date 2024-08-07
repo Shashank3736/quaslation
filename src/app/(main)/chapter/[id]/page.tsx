b2d1f040ae42c977299c307d655e0339f8c1f230
Shreyash Raj <shreyashr267@gmail.com>
2024-08-07 18:57:03 +0530

    allow premium users access

--- src/app/(main)/chapter/[id]/page.tsx
+++ src/app/(main)/chapter/[id]/page.tsx
@@ -2,16 +2,21 @@ import H3 from '@/components/typography/h3';
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 import { Button } from '@/components/ui/button';
 import { getChapter } from '@/lib/actions'
+import { PREMIUM_USERS } from '@/lib/config';
+import { auth } from '@clerk/nextjs/server';
 import { AlertCircle } from 'lucide-react';
 import Link from 'next/link';
 import React from 'react'
 
 export default async function ChapterPage({ params }: { params: { id: string }}) {
   const chapter = await getChapter(params.id);
+  const { userId } = auth();
+  let isPremium = false
+  if(userId !== null) isPremium = PREMIUM_USERS[userId]
   return (
     <div className='p-4'>
       <H3 className='mb-4'>Chapter {chapter.chapter}: {chapter.title}</H3>
-      {chapter.premium ? (
+      {(chapter.premium && !isPremium) ? (
         <Alert variant={"destructive"}>
           <AlertCircle className='h-4 w-4' />
           <AlertTitle>Premium Content</AlertTitle>
@@ -23,11 +28,21 @@ export default async function ChapterPage({ params }: { params: { id: string }})
         <div className='space-y-2 prose lg:prose-xl dark:prose-invert' dangerouslySetInnerHTML={{__html: chapter.content.html}} />
       )}
       <div className='flex justify-between mt-4'>
-        <Button disabled={chapter.previous == undefined}>{chapter.previous ? (<Link href={`/chapter/${chapter.previous.id}`}>Previous</Link>):"Previous"}</Button>
-        <Button variant={"secondary"}>
-          <Link href={`/novels/${chapter.novel.novel_slug.slug}`}>Index</Link>
-        </Button>
-        <Button disabled={chapter.next == undefined}>{chapter.next ? (<Link href={`/chapter/${chapter.next.id}`}>Next</Link>):"Next"}</Button>
+        {chapter.previous ? (
+          <Link href={`/chapter/${chapter.previous.id}`}><Button>Previous</Button></Link>
+        ):(
+          <Button disabled>Previous</Button>
+        )}
+        <Link href={`/novels/${chapter.novel.novel_slug.slug}`}>
+          <Button variant={"secondary"}>
+            Index
+          </Button>
+        </Link>
+        {chapter.next ? (
+          <Link href={`/chapter/${chapter.next.id}`}><Button>Next</Button></Link>
+        ):(
+          <Button disabled>Next</Button>
+        )}
       </div>
     </div>
   )
---
+++ src/lib/config.ts
@@ -0,0 +1,3 @@
+export const PREMIUM_USERS:{ [index: string]:boolean } = {
+  "user_2kGvnO7rByyxuXjW2la27ogZ8lf": true,
+}
