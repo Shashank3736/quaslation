818b5f785cec1336e5d4a256aece7759edc0917e
Shreyash Raj <shreyashr267@gmail.com>
2024-08-09 20:27:57 +0530

    add new css modules

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "./_css/globals.css";
+import "./_css/clerk.css"
 import { ThemeProvider } from "@/components/system/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 import { ClerkProvider } from "@clerk/nextjs";
