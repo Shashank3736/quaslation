d782b66607979cf72d2cad25c8ed1c8a42bb34bf
Shreyash Raj <shreyashr267@gmail.com>
2024-08-10 13:15:07 +0530

    updated with children props and changed position

--- src/components/shared/restricted-content.tsx
+++ src/components/shared/restricted-content.tsx
@@ -5,12 +5,13 @@
  */
 import { SignInButton } from "@clerk/nextjs"
 import { Button } from "../ui/button"
+import { ReactNode } from "react"
 
-export default function RestrictedContent() {
+export default function RestrictedContent({ children }: { children: ReactNode }) {
   return (
-    <div className="flex my-48 flex-col items-center justify-center bg-background px-4 py-12 sm:px-6 lg:px-8">
-      <div className="mx-auto max-w-md text-center">
-        <div className="inline-block rounded-lg bg-muted px-3 py-1 text-sm">Restricted Content</div>
+    <div className="flex relative flex-col bg-background">
+      <div className="mx-auto absolute text-center backdrop-blur-sm h-full w-full">
+        <div className="inline-block rounded-lg mt-48 bg-muted px-3 py-1 text-sm">Restricted Content</div>
         <h1 className="mt-4 text-3xl font-bold tracking-tight text-foreground sm:text-4xl">Access Restricted</h1>
         <p className="mt-4 text-muted-foreground">This content is restricted. Please log in to access it.</p>
         <div className="mt-6">
@@ -21,6 +22,9 @@ export default function RestrictedContent() {
             </Button>
           </SignInButton>
         </div>
+      </div>
+      <div className="px-3 overflow-auto max-h-[700px]">
+        {children}
       </div>
     </div>
   )
