e9759c7eea43d54f492ca7b83e243ab0ec0bddda
Shreyash Raj <shreyashr267@gmail.com>
2024-08-14 20:25:06 +0530

    solved issues with spacing

--- src/app/(main)/novels/[slug]/_components/volume.tsx
+++ src/app/(main)/novels/[slug]/_components/volume.tsx
@@ -31,9 +31,9 @@ export default function VolumeChapters({ volumeId }: { volumeId: string }) {
   }, [volumeId])
   return (
     <div>
-      <div className='flex flex-col mt-2'>
+      <div className='flex flex-col space-y-1'>
         {volume.volume.chapters.length > 0 ? volume.volume.chapters.map((chapter) => (
-          <div  key={chapter.id} className='mt-1 flex'>
+          <div  key={chapter.id} className='flex'>
             <Link href={`/chapter/${chapter.id}`} className='hover:underline'>{chapter.chapter}. {chapter.title}</Link>
             <Badge className={cn("ml-3", {"hidden": !chapter.premium})}>Premium</Badge>
           </div>
