13cdff7ebd0022dd0a244adf6ff3dc635246cf16
Shreyash Raj <shreyashr267@gmail.com>
2024-08-22 10:08:21 +0530

    redirect /chapter/:id, /novels/:novel/:chapter

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,18 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { getChapterSlug } from "./lib/actions";
+import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher([
   "/admin(.*)"
 ])
 
-export default clerkMiddleware((auth, req) => {
+export default clerkMiddleware(async (auth, req) => {
   if(!auth().userId && isProtectedRoute(req)) return auth().redirectToSignIn();
+  const url = new URL(req.url)
+  if(url.pathname.startsWith("/chapter")) {
+    const chapterSlugs = await getChapterSlug(url.pathname.split("/")[2]);
+    return NextResponse.redirect(new URL(`/novels/${chapterSlugs.novel.slug}/${chapterSlugs.slug}`, req.url));
+  }
 });
 
 export const config = {
