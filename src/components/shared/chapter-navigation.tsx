4bc0eb20d338adcb2ec23acf0eaacf033f3ca368
Shreyash Raj <shreyashr267@gmail.com>
2024-08-23 20:04:49 +0530

    Created chapter navigation with keyboards

---
+++ src/components/shared/chapter-navigation.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import { useRouter } from 'next/navigation';
+import { useCallback, useEffect } from 'react'
+
+export const ChapterNavigation = ({ previousLink, nextLink }: { previousLink?: string, nextLink?: string }) => {
+  const router = useRouter();
+  const handleKeyPress = useCallback((event:KeyboardEvent) => {
+    if(event.key == "ArrowLeft" && previousLink) {
+      router.push(previousLink)
+    } else if(event.key == "ArrowRight" && nextLink) {
+      router.push(nextLink)
+    }
+  }, [previousLink, nextLink, router]);
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyPress)
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress)
+    }
+  }, [handleKeyPress])
+  return null
+}
