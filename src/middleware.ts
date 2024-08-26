e1b255599dedc7461570eb051505e195f1b4ac29
Shreyash Raj <shreyashr267@gmail.com>
2024-08-26 15:23:18 +0530

    Restric /admin page

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,6 +8,7 @@ const isProtectedRoute = createRouteMatcher([
 
 export default clerkMiddleware(async (auth, req) => {
   if(!auth().userId && isProtectedRoute(req)) return auth().redirectToSignIn();
+  if((auth().userId !== process.env.ADMIN_USER_ID) && isProtectedRoute(req)) return NextResponse.redirect(new URL("/", req.url))
   const url = new URL(req.url)
   if(url.pathname.startsWith("/chapter")) {
     const chapterSlugs = await getChapterSlug(url.pathname.split("/")[2]);
