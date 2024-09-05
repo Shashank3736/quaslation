de3c16e4a60b5e4893c0eaaf1cb3eade64512622
Shreyash Raj <shreyashr267@gmail.com>
2024-09-05 15:49:13 +0530

    updated with prisma version

--- src/app/(main)/novels/page.tsx
+++ src/app/(main)/novels/page.tsx
@@ -1,18 +1,18 @@
 import H2 from '@/components/typography/h2'
 import { Separator } from '@/components/ui/separator'
-import { getNovels } from '@/lib/hygraph/query'
+import { getNovels } from '@/lib/prisma/query'
 import Link from 'next/link'
 import React from 'react'
 
 export default async function NovelList() {
-  const novels = await getNovels({})
+  const novels = await getNovels()
   return (
     <div className='p-4'>
       <H2 className='text-center'>List of Novels</H2>
       <Separator />
       <div className='flex flex-col mt-4'>
       {novels.map(novel => (
-        <Link className='mb-2 hover:underline text-blue-600 dark:text-primary' key={novel.id} href={`/novels/${novel.slug}`}>{novel.title}</Link>
+        <Link className='mb-2 hover:underline text-blue-600 dark:text-primary' key={novel.slug} href={`/novels/${novel.slug}`}>{novel.title}</Link>
       ))}
       </div>
     </div>
