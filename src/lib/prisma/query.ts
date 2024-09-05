d83501b052fdba9de0941b44f99c72d6a7d6cb1f
Shreyash Raj <shreyashr267@gmail.com>
2024-09-05 15:09:57 +0530

    created getLatestReleases

---
+++ src/lib/prisma/query.ts
@@ -0,0 +1,45 @@
+import "server-only";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+export const getLatestReleases = async ({ premium=false, take=10, skip=0 }) => {
+  const chapters = await prisma.chapter.findMany({
+    take,
+    skip,
+    orderBy: [
+      {
+        createdAt: "desc"
+      }
+    ],
+    select: {
+      publishedAt: true,
+      createdAt: true,
+      slug: true,
+      title: true,
+      number: true,
+      novel: {
+        select: {
+          title: true,
+          slug: true,
+        }
+      },
+      volume: {
+        select: {
+          number: true,
+          title: true,
+        }
+      },
+      content: {
+        select: {
+          text: true
+        }
+      }
+    },
+    where: {
+      premium
+    }
+  })
+  console.log(`Fetchded ${chapters.length} chapters.`)
+  return chapters;
+}
