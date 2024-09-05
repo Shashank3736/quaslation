b617c96209a822209502709e35b892deb5494d1d
Shreyash Raj <shreyashr267@gmail.com>
2024-09-05 21:11:19 +0530

    resolve error

--- src/app/(main)/novels/[slug]/[chapter]/page.tsx
+++ src/app/(main)/novels/[slug]/[chapter]/page.tsx
@@ -1,10 +1,15 @@
 import { ChapterPage } from '@/components/shared/chapter-page';
 import { getChapter } from '@/lib/prisma/query';
+import { notFound } from 'next/navigation';
 import React from 'react'
 
 export default async function Page({ params }: { params: { slug: string, chapter: string }}) {
-  const chapter = await getChapter(params.chapter);
-  return (
-    <ChapterPage chapter={chapter} />
-  )
+  try {
+    const chapter = await getChapter(params.chapter);
+    return (
+      <ChapterPage chapter={chapter} />
+    )
+  } catch (error) {
+    notFound()
+  }
 }
