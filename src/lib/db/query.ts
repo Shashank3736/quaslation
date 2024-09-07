7be29e4e8ad557f58c20a1accb2dda691d97b1e1
Shreyash Raj <shreyashr267@gmail.com>
2024-09-08 00:16:53 +0530

    added getNovelBySlug and getNovelChapters

--- src/lib/db/query.ts
+++ src/lib/db/query.ts
@@ -1,6 +1,6 @@
 import "server-only";
 import { db } from "."
-import { chapter, novel, richText } from "./schema";
+import { chapter, novel, richText, volume } from "./schema";
 import { desc, eq } from "drizzle-orm";
 
 export async function getReleases({ skip=0, premium=false }) {
@@ -24,4 +24,34 @@ export async function getNovelList() {
     title: novel.title,
     id: novel.id,
   }).from(novel).orderBy(novel.title)
+}
+
+export async function getNovelBySlug(slug: string) {
+  const data = await db.select({
+    id: novel.id,
+    description: richText.html,
+    title: novel.title,
+    thumbnail: novel.thumbnail,
+  }).from(novel).where(eq(novel.slug, slug)).innerJoin(richText, eq(novel.richTextId, richText.id))
+  return data[0]
+}
+
+export async function getNovelVolumes(novelId: number) {
+  return db.select().from(volume).where(eq(volume.novelId, novelId))
+}
+
+export const getNovelChapters = async({ novelId, skip=0, limit=25 }:{ novelId: number, skip?: number, limit?: number }) => {
+  return db.select({
+    slug: chapter.slug,
+    title: chapter.title,
+    volume: {
+      number: volume.number,
+      title: volume.title
+    },
+    number: chapter.number,
+  }).from(chapter)
+  .where(eq(chapter.novelId, novelId))
+  .leftJoin(volume, eq(chapter.volumeId, volume.id))
+  .orderBy(chapter.serial)
+  .offset(skip).limit(limit)
 }
