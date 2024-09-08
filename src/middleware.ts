3b9fea96ef92225a8af88bbef5c506eddbb105af
Shreyash Raj <shreyashr267@gmail.com>
2024-09-08 09:59:09 +0530

    redirect for first and last chapter of novel

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { getChapterSlug } from "./lib/hygraph/query";
 import { NextResponse } from "next/server";
+import { getNovelFirstChapter, getNovelLastChapter } from "./lib/db/query";
 
 const isProtectedRoute = createRouteMatcher([
   "/admin(.*)"
@@ -15,6 +16,21 @@ export default clerkMiddleware(async (auth, req) => {
   if(url.pathname.startsWith("/chapter")) {
     const chapterSlugs = await getChapterSlug(url.pathname.split("/")[2]);
     return NextResponse.redirect(new URL(`/novels/${chapterSlugs.novel.slug}/${chapterSlugs.slug}`, req.url));
+  }
+  if(url.pathname.startsWith("/_series")) {
+    const [_1, _2, novelId, pos] = url.pathname.split("/");
+    console.log(novelId)
+    let chapter = {
+      slug:"",
+      novel:""
+    }
+    if(pos === "0") {
+      chapter = await getNovelFirstChapter(parseInt(novelId));
+    } else {
+      chapter = await getNovelLastChapter(parseInt(novelId));
+    }
+
+    return NextResponse.redirect(new URL(`/novels/${chapter.novel}/${chapter.slug}`, req.url));
   }
 });
 
