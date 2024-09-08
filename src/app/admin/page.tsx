bc8d04cc98994e2c9c65ef8ab0e73e2f66416a7d
Shreyash Raj <shreyashr267@gmail.com>
2024-09-08 13:36:37 +0530

    This now contains novels list

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,14 +1,25 @@
 import H1 from '@/components/typography/h1'
-import React, { Suspense } from 'react'
-import { NovelListSkeleton, NovelList } from './novel-list'
+import { Separator } from '@/components/ui/separator'
+import Link from 'next/link'
+
+const links = [{
+  tag: "Back to Main",
+  href: "/"
+}, {
+  tag: "Chapters",
+  href: "/admin/chapters"
+}]
 
 const AdminPage = () => {
   return (
     <div className='m-8'>
-      <H1 className='text-center mb-4'>Novels</H1>
-      <Suspense fallback={<NovelListSkeleton />}>
-        <NovelList />
-      </Suspense>
+      <H1>Admin Page</H1>
+      <Separator className='my-4' />
+      <div className='flex flex-wrap'>
+      {links.map(link => (
+        <Link key={link.tag} href={link.href} className='py-4 px-8 m-2 border rounded-lg hover:bg-secondary'>{link.tag}</Link>
+      ))}
+      </div>
     </div>
   )
 }
