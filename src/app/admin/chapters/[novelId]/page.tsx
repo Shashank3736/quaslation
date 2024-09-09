3a7a38c9e3668a3da24b79e4e06f773aea7f79a6
Shreyash Raj <shreyashr267@gmail.com>
2024-09-09 12:47:38 +0530

    link to create chapter for the novel

--- src/app/admin/chapters/[novelId]/page.tsx
+++ src/app/admin/chapters/[novelId]/page.tsx
@@ -2,6 +2,8 @@ import { getChapters } from '@/lib/db/query';
 import { notFound } from 'next/navigation';
 import React from 'react'
 import { ChaptersTable } from '../chapters-table';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 const AdminNovelChapters = async({ params, searchParams }:{ params: { novelId: string }, searchParams: { page?: string }}) => {
   const novelId = parseInt(params.novelId);
@@ -11,6 +13,7 @@ const AdminNovelChapters = async({ params, searchParams }:{ params: { novelId: s
   const chapters = await getChapters({ novelId, skip: (page-1)*25, limit: 25 });
   return (
     <div className='m-4'>
+      <Button asChild><Link href={`${novelId}/create`}>Create Chapter +</Link></Button>
       <ChaptersTable data={chapters} />
     </div>
   )
