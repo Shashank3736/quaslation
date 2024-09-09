8936cb40dd652e98663d4d9a122f037df5a4f596
Shreyash Raj <shreyashr267@gmail.com>
2024-09-09 12:45:03 +0530

    added slugify

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -55,6 +55,15 @@ export interface DiscordEmbed {
   fields?: EmbedField[];
 }
 
+export function slugify(str: string) {
+  str = str.replace(/^\s+|\s+$/g, ''); // trim leading/trailing white space
+  str = str.toLowerCase(); // convert string to lowercase
+  str = str.replace(/[^a-z0-9 -]/g, '') // remove any non-alphanumeric characters
+           .replace(/\s+/g, '-') // replace spaces with hyphens
+           .replace(/-+/g, '-'); // remove consecutive hyphens
+  return str;
+}
+
 export function createDiscordEmbed({
   title,
   description,
