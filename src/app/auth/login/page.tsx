12f88801f63a79d6fb74aef4373c4da7bfef941d
Shreyash Raj <shreyashr267@gmail.com>
2024-09-12 23:22:17 +0530

    updated login page with oauth signin

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,14 +1,7 @@
-import { login, signup } from "./actions";
+import SignInWithDiscord from "@/components/system/sign-in-discord";
 
-export default function LoginPage() {
+export default function LoginPage({ searchParams }:{ searchParams: { next?: string }}) {
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
+    <SignInWithDiscord next={searchParams.next || "/"} />
   )
 }
