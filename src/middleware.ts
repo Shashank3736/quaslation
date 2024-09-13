9a5da0fd6bddc20247f30debbe5c1e70b2580dc0
Shreyash Raj <shreyashr267@gmail.com>
2024-09-14 01:24:45 +0530

    removed comments

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,5 @@
-// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-// import { getChapterSlug } from "./lib/_hygraph/query";
 import { NextRequest } from "next/server";
-// import { getNovelFirstChapter, getNovelLastChapter } from "./lib/db/query";
 import { updateSession } from "./lib/supabase/middleware";
-
-// const isProtectedRoute = createRouteMatcher([
-//   "/admin(.*)"
-// ])
 
 export async function middleware(request: NextRequest) {
   return await updateSession(request)
