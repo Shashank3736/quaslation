ddbffb16a13a7582c6b34b60a4ac1098e4eb27f9
Shreyash Raj <shreyashr267@gmail.com>
2024-09-14 03:22:32 +0530

    admin/novels/create page

---
+++ src/app/admin/novels/create/page.tsx
@@ -0,0 +1,14 @@
+import H2 from '@/components/typography/h2'
+import React from 'react'
+import CreateNovelForm from './create-novel-form'
+
+const AdminNovelCreate = () => {
+  return (
+    <div>
+      <H2>Create New Novel</H2>
+      <CreateNovelForm />
+    </div>
+  )
+}
+
+export default AdminNovelCreate
