bee036f572d3209896b893b13a27649e40d253bc
Shreyash Raj <shreyashr267@gmail.com>
2024-09-18 03:09:57 +0530

    improved caching

--- src/app/_components/actions.ts
+++ src/app/_components/actions.ts
@@ -1,5 +1,9 @@
 "use server";
 
 import { getReleases } from "@/lib/db/query";
+import { unstable_cache } from "next/cache";
 
-export const action = getReleases
+export const action = unstable_cache(getReleases, [], {
+  tags: ["chapter_update"],
+  revalidate: 3600
+})
