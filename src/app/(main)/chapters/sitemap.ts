31d458eea001e1c76bb04e9026b6e8271b264abb
Shreyash Raj <shreyashr267@gmail.com>
2024-09-18 03:06:37 +0530

    /chapters/sitemap.ts support caching

--- src/app/(main)/chapters/sitemap.ts
+++ src/app/(main)/chapters/sitemap.ts
@@ -2,14 +2,26 @@ import { db } from "@/lib/db";
 import { chapter as chapterTable, novel as novelTable } from "@/lib/db/schema";
 import { eq, isNotNull } from "drizzle-orm";
 import { MetadataRoute } from "next";
+import { unstable_cache } from "next/cache";
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const base = "https://quaslation.xyz"
-  const chapters = await db.select({
+const getCache = unstable_cache(async () => {
+  console.log("Here");
+  return await db.select({
     novel: novelTable.slug,
     chapter: chapterTable.slug,
     updatedAt: chapterTable.updatedAt
-  }).from(chapterTable).where(isNotNull(chapterTable.publishedAt)).innerJoin(novelTable, eq(chapterTable.novelId, novelTable.id));
+  })
+  .from(chapterTable)
+  .where(isNotNull(chapterTable.publishedAt))
+  .innerJoin(novelTable, eq(chapterTable.novelId, novelTable.id));
+}, [], {
+  tags: ["chapter_publish"],
+  revalidate: 24*3600
+});
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const base = "https://quaslation.xyz"
+  const chapters = await getCache();
   
   return chapters.map(chapter => (
     {
