e21d105dfbfbcf07a4c8dcebfc37a4bb9cb6bd98
Shreyash Raj <shreyashr267@gmail.com>
2024-09-17 14:11:16 +0530

    v2.3.2: revalidate tags at publish and free chapter

--- src/app/admin/chapters/actions.ts
+++ src/app/admin/chapters/actions.ts
@@ -1,14 +1,17 @@
 "use server";
 
 import { freeChapters, publishChapters } from "@/lib/db/query";
-import { revalidatePath } from "next/cache";
+import { revalidatePath, revalidateTag } from "next/cache";
 
 export const freeChapter = async (novelId: number, serial: number) => {
   await freeChapters({ novelId, first: serial, last: serial });
-  revalidatePath(`/admin/chapters/${novelId}`)
+  revalidatePath(`/admin/chapters/${novelId}`);
+  revalidateTag("chapter");
+  revalidatePath("chapters");
 }
 
 export const publish = async ({ novelId, serial }:{ novelId: number, serial: number}) => {
   await publishChapters({ novelId, serial });
   revalidatePath(`/admin/chapters/${novelId}`);
+  revalidateTag("chapter")
 };
