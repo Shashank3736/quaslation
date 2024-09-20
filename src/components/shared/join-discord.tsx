ce393766408ea060706108975fc4228b67cca774
Shreyash Raj <shreyashr267@gmail.com>
2024-09-20 23:10:46 +0530

    updated ui

--- src/components/shared/join-discord.tsx
+++ src/components/shared/join-discord.tsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import { Alert, AlertDescription, AlertTitle } from '../ui/alert'
-import { Terminal } from 'lucide-react'
+import { MessageCircle, Terminal } from 'lucide-react'
 import { Button } from '../ui/button'
 import { DISCORD_INVITE_URL } from '@/lib/config'
 
 const JoinDiscord = () => {
   return (
-    <Alert className='m-4 w-fit'>
-      <Terminal className='h-4 w-4' />
+    <Alert className='m-4 space-x-2 w-fit rounded-none rounded-r-lg border-l-8 dark:border-blue-400 border-blue-600'>
+      <MessageCircle className='size-6 dark:stroke-blue-400 stroke-blue-600' />
       <AlertTitle>Join our Discord Community</AlertTitle>
       <AlertDescription>
         <p className='mb-2'>
           Connect with like-minded individuals, participate in discussions, and stay up-to-date with the latest news and updates.
           You can also request for translation of other novels.
         </p>
-        <Button asChild><a href={DISCORD_INVITE_URL}>Join Now</a></Button>
+        <Button className='bg-blue-600 dark:bg-blue-400 no-underline' asChild><a href={DISCORD_INVITE_URL}>Join Now</a></Button>
       </AlertDescription>
     </Alert>
   )
