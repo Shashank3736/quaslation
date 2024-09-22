b21952f01ef63bc6b192c15f0c80743da4ad6899
Shreyash Raj <shreyashr267@gmail.com>
2024-09-22 11:28:28 +0530

    updated footer links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -18,6 +18,10 @@ const footerLinks = [
     name: "Terms of Service",
     href: "/terms-of-service",
   },
+  {
+    name: "Comment Policy",
+    href: "/comment-policy"
+  }
 ];
 
 export function FooterComponent() {
@@ -25,14 +29,11 @@ export function FooterComponent() {
     <footer className="w-full border-t bg-background">
       <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-          <Link href="/" className="flex items-center space-x-2">
-            <span className="text-lg font-bold">Quaslation</span>
-          </Link>
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
             &copy; {new Date().getFullYear()} Quaslation. All rights reserved.
           </p>
         </div>
-        <nav className="flex gap-4 sm:gap-6">
+        <nav className="grid grid-cols-2 md:grid-cols-3 lg:flex gap-4 sm:gap-6">
           {footerLinks.map((link) => (
             <Link className="text-sm font-medium link" key={link.href} href={link.href}>
               {link.name}
