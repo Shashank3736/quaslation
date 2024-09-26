958024854abb45d69b5eea2c86056e5e9c8944ac
Shreyash Raj <shreyashr267@gmail.com>
2024-09-26 23:48:24 +0530

    remove unused imports

--- src/app/admin/novel/create/create-novel-form.tsx
+++ src/app/admin/novel/create/create-novel-form.tsx
@@ -15,7 +15,6 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { Textarea } from '@/components/ui/textarea'
 import { toast } from '@/components/ui/use-toast'
 import Link from 'next/link'
 import { createNovel } from './actions'
