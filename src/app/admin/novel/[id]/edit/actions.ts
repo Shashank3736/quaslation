5356a61879e956f4e568364e8a5c71e52d98c038
Shreyash Raj <shreyashr267@gmail.com>
2024-09-26 23:06:49 +0530

    updateNovel action

---
+++ src/app/admin/novel/[id]/edit/actions.ts
@@ -0,0 +1,36 @@
+"use server";
+
+import { z } from "zod";
+import { editNovelSchema } from "./edit-novel-form";
+import { db } from "@/lib/db";
+import { richText, novel as novelTable } from "@/lib/db/schema";
+import { markdownToHtml, markdownToText } from "@/lib/utils";
+import { eq } from "drizzle-orm";
+import { revalidateTag } from "next/cache";
+
+export const updateNovel = async (values: z.infer<typeof editNovelSchema>) => {
+  try {
+    await db.update(richText).set({
+      markdown: values.description,
+      html: await markdownToHtml(values.description),
+      text: await markdownToText(values.description)
+    }).where(eq(richText.id, values.richTextId));
+  
+    await db.update(novelTable).set({
+      title: values.title,
+      thumbnail: values.thumbnail == "" ? undefined : values.thumbnail,
+    }).where(eq(novelTable.id, values.id))
+
+    revalidateTag("novel:update");
+    revalidateTag(`novel:update:${values.slug}`);
+
+    return {
+      message: "ok"
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      message: `Error: ${error}`
+    }
+  }
+}
