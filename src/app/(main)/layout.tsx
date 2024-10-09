3025497ec688234ce826299020c7889ea5b0aec7
Shreyash Raj <shreyashr267@gmail.com>
2024-10-09 14:40:21 +0530

    added google ads support

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Navbar from '../_components/navbar'
-// import GoogleAdsense from '@/components/system/google-adsense'
+import GoogleAdsense from '@/components/system/google-adsense'
 import { FooterComponent } from '@/components/footer'
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="max-w-[1024px] mx-auto flex flex-col min-h-screen">
-      {/* <GoogleAdsense pId={process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_ID || ""} /> */}
+      <GoogleAdsense pId={process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_ID || ""} />
       <Navbar />
       <main className='flex-grow mx-4'>
         {children}
