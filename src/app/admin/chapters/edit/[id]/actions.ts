eab8e435fda06ff40289ab76a90f82a5086910b1
Shreyash Raj <shreyashr267@gmail.com>
2025-06-01 12:33:21 +0530

    Enhance chapter editing functionality with improved form validation and error handling

--- src/app/admin/chapters/edit/[id]/actions.ts
+++ src/app/admin/chapters/edit/[id]/actions.ts
@@ -1,10 +1,12 @@
 "use server";
 
 import { db } from "@/lib/db";
-import { chapter as chapterTable, novel as novelTable, richText as richTextTable, volume as volumeTable } from "@/lib/db/schema";
+import { chapter, chapter as chapterTable, novel as novelTable, richText as richTextTable, volume as volumeTable } from "@/lib/db/schema";
 import { markdownToHtml, markdownToText } from "@/lib/utils";
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 import { revalidateTag } from "next/cache";
+import { z } from "zod";
+import { formSchema } from "./form";
 
 export const getChapter = async(id:number) => {
   return (await db.select({
@@ -15,21 +17,57 @@ export const getChapter = async(id:number) => {
     richText: {
       id: richTextTable.id,
       content: richTextTable.markdown,
-    }
+    },
+    serial: chapterTable.serial,
+    volume: volumeTable.number,  
   })
   .from(chapterTable)
   .innerJoin(richTextTable, eq(chapterTable.richTextId, richTextTable.id))
+  .innerJoin(volumeTable, eq(chapterTable.volumeId, volumeTable.id))
   .where(eq(chapterTable.id, id))
   )[0]
 }
 
-export const updateChapterContent = async(richTextId: number, markdown: string, chapterSlug: string) => {
-  const text = await markdownToText(markdown);
-  const html = await markdownToHtml(markdown);
+export const updateChapterContent = async(richTextId: number, values: z.infer<typeof formSchema>, chapterSlug: string) => {
+  const text = await markdownToText(values.content);
+  const html = await markdownToHtml(values.content);
 
   await db.update(richTextTable).set({
-    text, html, markdown
+    text, html, markdown: values.content
   }).where(eq(richTextTable.id, richTextId))
+
+  const novel = await db.query.chapter.findFirst({
+    where: (chapter, { eq }) => eq(chapter.slug, chapterSlug),
+    columns: {
+      novelId: true,
+    },
+  });
+
+  if (!novel) {
+    throw new Error("Novel not found for the given chapter slug.");
+  }
+
+  const volume = await db.query.volume.findFirst({
+    where: (volume, { eq, and }) => and(
+      eq(volume.novelId, novel.novelId),
+      eq(volume.number, values.volume)
+    ),
+    columns: {
+      id: true,
+    },
+  });
+
+  if (!volume) {
+    throw new Error("Volume not found for the given chapter slug.");
+  }
+
+  await db.update(chapterTable).set({
+    title: values.title,
+    serial: values.serial,
+    number: values.number,
+    volumeId: volume.id,
+  }).where(eq(chapterTable.slug, chapterSlug));
+
   revalidateTag(`chapter:update:${chapterSlug}`);
   revalidateTag(`chapter:update:richText:${chapterSlug}`);
   revalidateTag(`chapter:update:content:${chapterSlug}`);
--- src/app/admin/chapters/edit/[id]/form.tsx
+++ src/app/admin/chapters/edit/[id]/form.tsx
@@ -20,11 +20,14 @@ import { toast } from '@/components/ui/use-toast';
 import AutoResizeTextarea from '@/components/auto-resize-textarea';
 import { markdownToHtml } from '@/lib/utils';
 
-const formSchema = z.object({
+export const formSchema = z.object({
   title: z.string(),
   content: z.string().min(1, {
     message: "Bro you need it fill it na."
   }),
+  serial: z.coerce.number(),
+  number: z.coerce.number(),
+  volume: z.coerce.number(),
 });
 
 export const EditChapterForm = ({ data }:{ data: Awaited<ReturnType<typeof getChapter>>}) => {
@@ -33,22 +36,31 @@ export const EditChapterForm = ({ data }:{ data: Awaited<ReturnType<typeof getCh
     defaultValues: {
       title: data.title,
       content: data.richText.content,
+      serial: data.serial,
+      number: data.number,
+      volume: data.volume,
     }
-  })
+  });
 
-  const [preview, setPreview] = useState("")
+  const [preview, setPreview] = useState("");
   const [submiting, setSubmiting] = useState(false);
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    if(data.richText.content !== values.content) {
-      setSubmiting(true)
-      await updateChapterContent(data.richText.id, values.content, data.slug);
+    setSubmiting(true);
+    try {
+      await updateChapterContent(data.richText.id, values, data.slug);
       toast({
-        description: "Chapter updated successfully!"
-      })
-      setSubmiting(false)
+      description: "Chapter updated successfully!"
+      });
+    } catch (error) {
+      toast({
+      description: "Failed to update chapter.",
+      variant: "destructive"
+      });
     }
-  }
+    setSubmiting(false);
+  };
+
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8 m-4'>
@@ -66,6 +78,48 @@ export const EditChapterForm = ({ data }:{ data: Awaited<ReturnType<typeof getCh
             </FormItem>
           )} 
         />
+        <FormField
+          control={form.control}
+          name="serial"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Serial</FormLabel>
+              <FormControl>
+                <Input placeholder='Chapter position' {...field} />
+              </FormControl>
+              <FormDescription>Must be unique for the novel and used in deciding navigation.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )} 
+        />
+        <FormField
+          control={form.control}
+          name="number"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Chapter</FormLabel>
+              <FormControl>
+                <Input placeholder='Write chapter number' {...field} />
+              </FormControl>
+              <FormDescription>Unique for volume.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )} 
+        />
+        <FormField
+          control={form.control}
+          name="volume"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Volume</FormLabel>
+              <FormControl>
+                <Input placeholder='Volume number' {...field} />
+              </FormControl>
+              <FormDescription>Volume number of the novel.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )} 
+        />
         <div className="grid grid-cols-2 space-x-2">
           <FormField
             control={form.control}
@@ -78,9 +132,9 @@ export const EditChapterForm = ({ data }:{ data: Awaited<ReturnType<typeof getCh
                     placeholder='Write markdown here...' 
                     {...field} 
                     onChange={(e) => {
-                      field.onChange(e)
+                      field.onChange(e);
                       markdownToHtml(e.target.value)
-                      .then(data => setPreview(data));
+                        .then(data => setPreview(data));
                     }}
                   />
                 </FormControl>
@@ -97,8 +151,8 @@ export const EditChapterForm = ({ data }:{ data: Awaited<ReturnType<typeof getCh
             />
           </div>
         </div>
-        <Button type="submit">Submit</Button>
+        <Button type="submit" disabled={submiting}>Submit</Button>
       </form>
     </Form>
-  )
-}
+  );
+};
